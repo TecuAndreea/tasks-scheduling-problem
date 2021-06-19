144f4ef6b6a05f3bddc174d84837358927237455
TecuAndreea <tecu_andreea@yahoo.com>
2021-06-19 11:45:30 +0300

    Project creation

---
+++ TaskSchedulingProblem/TaskSchedulingProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskSchedulingProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
